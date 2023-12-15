1ec6bf4ba90f62c85a0960637281a0de7e4c16da
viresh <40684172+VireshHiremath@users.noreply.github.com>
2023-12-15 14:21:41 +0530

    Updated

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -5,3 +5,11 @@ def add(x,y){
 def mul(x,y){
   echo "Multiplication of ${x} and ${y} is ${x*y}"
 }
+
+def sub(x, y){
+  echo "Difference of ${x} and ${y} is ${x-y}"
+}
+
+def power(x, y){
+  echo "${x} to the power of ${y} is ${Math.pow(x,y)}"
+}
